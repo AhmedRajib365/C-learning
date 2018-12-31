41ca6e2d6b311a9d525844112481271deecc0faf
Ahmed Rajib <md.rajib2424@gmail.com>
2018-12-31 23:30:11 +0600

    Create Arithmetic_Calculation.c

---
+++ Arithmetic_Calculation.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int i=9, j=3, k;
+  k=i+j; // addition +
+  printf("The addition of %d and %d is %d \n", i, j, k);
+  k=i-j; // Subtraction -
+  printf("The Subtraction of %d and %d is %d \n", i, j, k);
+  k=i*j; // multiplication *
+  printf("The multiplication of %d and %d is %d \n", i, j, k);
+  k=i/j; // division /
+  printf("The division of %d and %d is %d \n", i, j, k);
+
+  return 0;
+
+
+
+}
