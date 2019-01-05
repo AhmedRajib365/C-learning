3a7459a5f7ae120f5f70a1fc1164b0891ce960a7
Ahmed Rajib <md.rajib2424@gmail.com>
2019-01-05 21:47:49 +0600

    Create Relational_Operators.c

---
+++ Relational_Operators.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+// Relational Operators
+int main()
+{
+  //int result = 5>3; res: 1
+  //int result = 5<3; // res 0                  // true means = 1 , false means = 0;
+  //int result = 3==7;   res 0                  ! means not equal #
+  //int result = 3<=7;   res 1
+  //int result = 3<=3;   res 0
+  //int result = 3!=3;   res 0
+  int result = 3!=4;
+  printf("%d", result);
+
+  /*
+        <, >, =>, <=, ==, !=,
+  */
+    
+   return 0;
+}
